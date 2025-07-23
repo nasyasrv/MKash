a0d9e16068542fad754cb43e9ca5e193b0d6d672
nasyasrv <asrivanasya0@gmail.com>
2025-07-22 18:10:23 -1200

    one

---
+++ resources/views/interface/layout/navbar2.blade.php
@@ -0,0 +1,77 @@
+ <aside id="layout-menu" class="layout-menu-horizontal menu-horizontal menu bg-menu-theme flex-grow-0">
+              <div class="container-xxl d-flex h-100">
+                <ul class="menu-inner pb-2 pb-xl-0">
+                  <!-- Dashboards -->
+                  <li class="menu-item active">
+                    <a href="#" class="menu-link ">
+                      <i class="menu-icon tf-icons ti ti-smart-home"></i>
+                      <div data-i18n="Dashboards">Dashboards</div>
+                    </a>
+                  </li>
+
+                  <!-- Layouts -->
+                  <li class="menu-item">
+                    <a href="#" class="menu-link ">
+                      <i class="menu-icon tf-icons ti ti-layout-sidebar"></i>
+                      <div data-i18n="Layouts">Layouts</div>
+                    </a>
+                  </li>
+
+                  <!-- Apps -->
+                  <li class="menu-item">
+                    <a href="#" class="menu-link ">
+                      <i class="menu-icon tf-icons ti ti-layout-grid-add"></i>
+                      <div data-i18n="Apps">Apps</div>
+                    </a>
+                  </li>
+
+                  <!-- Pages -->
+                  <li class="menu-item">
+                    <a href="#" class="menu-link ">
+                      <i class="menu-icon tf-icons ti ti-file"></i>
+                      <div data-i18n="Pages">Pages</div>
+                    </a>
+                  </li>
+
+                  <!-- Components -->
+                  <li class="menu-item">
+                    <a href="#" class="menu-link ">
+                      <i class="menu-icon tf-icons ti ti-toggle-left"></i>
+                      <div data-i18n="Components">Components</div>
+                    </a>
+                  </li>
+
+                  <!-- Forms -->
+                  <li class="menu-item">
+                    <a href="#" class="menu-link ">
+                      <i class="menu-icon tf-icons ti ti-forms"></i>
+                      <div data-i18n="Forms">Forms</div>
+                    </a>
+                  </li>
+
+                  <!-- Tables -->
+                  <li class="menu-item">
+                    <a href="#" class="menu-link ">
+                      <i class="menu-icon tf-icons ti ti-layout-grid"></i>
+                      <div data-i18n="Tables">Tables</div>
+                    </a>
+                  </li>
+
+                  <!-- Charts & Maps -->
+                  <li class="menu-item">
+                    <a href="#" class="menu-link ">
+                      <i class="menu-icon tf-icons ti ti-chart-bar"></i>
+                      <div data-i18n="Charts & Maps">Charts & Maps</div>
+                    </a>
+                  </li>
+
+                  <!-- Misc -->
+                  <li class="menu-item">
+                    <a href="#" class="menu-link ">
+                      <i class="menu-icon tf-icons ti ti-box-multiple"></i>
+                      <div data-i18n="Misc">Misc</div>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </aside>
