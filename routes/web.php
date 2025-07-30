d7f37ced8fe47fb37a2e1cf7b8480dc759ca22a2
nasyasrv <asrivanasya0@gmail.com>
2025-07-29 14:55:47 -1200

    admin

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/home_admin', function () {
+    return view('admin.home');
+});
